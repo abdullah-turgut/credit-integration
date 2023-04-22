6fdb950fdb4a4bb7c4e5d4ae7a195bdfd329bf3b
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-22 08:19:12 +0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,11 @@
 import LoginForm from '../components/LoginForm';
 export default function Login() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 relative">
       <LoginForm />
+      <p className="absolute top-0 left-0 font-thin text-2xl text-white py-4 px-8">
+        Credit Integration.
+      </p>
     </div>
   );
 }
