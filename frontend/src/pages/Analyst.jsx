4b0c8aacccfc293d7e42a77e226404ecbaf6507c
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-22 11:26:25 +0300

    kullanılmayan kodlar temizlendi

--- frontend/src/pages/Analyst.jsx
+++ frontend/src/pages/Analyst.jsx
@@ -1,7 +1,5 @@
-import { MainContext, useContext } from '../contexts/MainContext';
 import Layout from '../components/Layout';
 export default function Analyst() {
-  const { user } = useContext(MainContext);
   return (
     <div>
       <Layout />
--- frontend/src/pages/SuperAdmin.jsx
+++ frontend/src/pages/SuperAdmin.jsx
@@ -1,11 +1,6 @@
-import { MainContext, useContext } from '../contexts/MainContext';
 import Layout from '../components/Layout';
 
 export default function SuperAdmin() {
-  const { user } = useContext(MainContext);
-
-  console.log(user);
-
   return (
     <div>
       <Layout />
