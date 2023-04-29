0abbecf39acfc776080d5184b1849cd2da43baea
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-29 18:42:07 +0300

    Update dataFormat.js

--- frontend/src/helpers/dataFormat.js
+++ frontend/src/helpers/dataFormat.js
@@ -1,23 +1,25 @@
 import { nanoid } from 'nanoid';
 
 function formatData(arr) {
-  return arr.map((item) => {
-    return {
-      user_id: item['#'],
-      username: item['İsim Soyisim'],
-      job: item['Hangi Pozisyonda Çalışıyorsun?'],
-      field: item['Sektör'],
-      education: item['Son Mezuniyet'],
-      startYear: item['Çalışmaya ilk başladığın yıl nedir?'],
-      PS: item['PS'],
-      SS: item['SectorScore'],
-      creditScore: (
-        item['PS'] * 0.4 +
-        item['SectorScore'] * 0.5 -
-        (2023 - Number(item['Çalışmaya ilk başladığın yıl nedir?'])) * 0.1
-      ).toFixed(1),
-    };
-  });
+  return arr
+    .map((item) => {
+      return {
+        user_id: item['#'],
+        username: item['İsim Soyisim'],
+        job: item['Hangi Pozisyonda Çalışıyorsun?'],
+        field: item['Sektör'],
+        education: item['Son Mezuniyet'],
+        startYear: item['Çalışmaya ilk başladığın yıl nedir?'],
+        PS: item['PS'],
+        SS: item['SectorScore'],
+        creditScore: (
+          item['PS'] * 0.4 +
+          item['SectorScore'] * 0.5 -
+          (2023 - Number(item['Çalışmaya ilk başladığın yıl nedir?'])) * 0.1
+        ).toFixed(1),
+      };
+    })
+    .filter((item) => item.startYear >= 2015 && item.education !== 'Lise');
 }
 
 export { formatData };
