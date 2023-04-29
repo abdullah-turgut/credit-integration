0cadb2d9e3d3f9517f2d497f6629faab3c804fb4
abdullah-turgut <abdullah.turgut.94@gmail.com>
2023-04-29 17:59:49 +0300

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -13,7 +13,7 @@ export default function Layout() {
   }
 
   return (
-    <div className="fixed left-0 top-0  h-screen w-[300px] flex flex-col px-5 py-10 border-r border-gray-300">
+    <div className="fixed left-0 top-0  h-screen w-[300px] flex flex-col px-5 py-10 border-r border-gray-300 bg-white z-20">
       <div className="flex flex-col gap-y-3 text-right border-b pb-5 text-violet-800">
         <p className="text-4xl font-medium ">Hoşgeldin</p>
         <p className="text-xl truncate  font-thin">{user.email}</p>
